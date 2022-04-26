c61335a82c6fc43ea69fd1d5a88deb09c6fdb5b5
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-04-27 00:18:26 +0530

    Time: 575 ms (5.83%), Space: 156.7 MB (8.33%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,7 +1,66 @@
 class Solution {
 public:
+        void merge(vector<int>& nums,int s,int e)
+        {
+                int i=s;
+                int m=(s+e)/2;
+                int j=m+1;
+                
+                vector<int>temp;
+                
+              while(i<=m&&j<=e)
+                 {
+                 if(nums[i]<nums[j])
+                 {
+                 temp.push_back(nums[i]);   
+                 i++;
+                }
+               else
+                 {
+                 temp.push_back(nums[j]);
+                 j++;
+               }
+              }
+                      
+                      while(i<=m)
+                      {
+                              temp.push_back(nums[i]);
+                              i++;
+                      }
+                      while(j<=e)
+                      {
+                              temp.push_back(nums[j]);
+                              j++;
+                      }
+                      
+                      int k=0;
+                   for(int idx=s;idx<=e;idx++)
+                   {
+                           nums[idx]=temp[k++];
+                   }
+                      
+                     return; 
+            }
+
+        void mergesort(vector<int>& nums,int s,int e)
+        {
+        if(s>=e)
+        {
+                return;
+        }
+                int mid=(s+e)/2;
+                mergesort(nums,s,mid);
+                mergesort(nums,mid+1,e);
+                
+                 return merge(nums,s,e);
+                
+        }
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+            int s=0;
+            int e=nums.size()-1;
+            
+       mergesort(nums,s,e);
             return nums;
+            
     }
 };
