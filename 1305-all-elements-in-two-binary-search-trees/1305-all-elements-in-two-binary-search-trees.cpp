04b354dbf451b44478d8db6d6fe0353ca4d0424e
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-04-30 19:34:37 +0530

    Time: 174 ms (67.42%), Space: 87.6 MB (6.64%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -10,25 +10,52 @@
  * };
  */
 class Solution {
-        private:
-        void solve(TreeNode* root,vector<int>&ans)
-        {
-             if(root==nullptr)
-             {
-                     return;
-             }
-                ans.push_back(root->val);
-                solve(root->left,ans);
-                solve(root->right,ans);
-                
-                
-        }
 public:
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int>ans;
-            solve(root1,ans);
-            solve(root2,ans);
-            sort(ans.begin(),ans.end());
-            return ans;
-    }
+        
+    void inorder(TreeNode* root, vector<int> &arr){
+       if(root==nullptr)
+       {
+               return;
+       }
+          
+          
+           inorder(root->left, arr);
+             arr.push_back(root->val);
+           inorder(root->right, arr);
+   }
+   
+   void merge(vector<int> arr1, vector<int> arr2, vector<int> &ans){
+       int i=0, j=0;
+       
+       while(i<arr1.size() && j<arr2.size()){
+           if(arr1[i] <arr2[j]){
+               ans.push_back(arr1[i]);
+               i++;
+           } else{
+               ans.push_back(arr2[j]);
+               j++;
+           }
+       }
+       
+       while(i<arr1.size()){
+           ans.push_back(arr1[i]);
+           i++;
+       }
+       
+       while(j<arr2.size()){
+           ans.push_back(arr2[j]);
+           j++;
+       }
+   }
+   
+   
+   
+   vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+       vector<int> arr1, arr2, ans;
+       inorder(root1, arr1);
+       inorder(root2, arr2);
+       merge(arr1, arr2, ans);
+       return ans;
+   }
 };
+    
