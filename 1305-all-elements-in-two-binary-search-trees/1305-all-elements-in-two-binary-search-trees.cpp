ca6e9102f5fc6e290add09f546d927ccc65f8797
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-04-30 23:00:35 +0530

    Time: 175 ms (66.57%), Space: 87.5 MB (7.03%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -10,52 +10,55 @@
  * };
  */
 class Solution {
+        private:
+        void inorder(TreeNode* root,vector<int>&ans)
+        {
+                if(root==nullptr)
+                {
+                        return;
+                }
+                inorder(root->left,ans);
+                ans.push_back(root->val);
+                inorder(root->right,ans);
+        }
+        void merge(vector<int>ans1,vector<int>ans2,vector<int>&result)
+        {
+                int i=0;
+                int j=0;
+                while(i<ans1.size()&&j<ans2.size())
+                {
+                if(ans1[i]<ans2[j])
+                {
+                        result.push_back(ans1[i]);
+                        i++;
+                }
+                else
+                {
+                        result.push_back(ans2[j]);
+                        j++;
+                }
+                }
+                
+                while(i<ans1.size())
+                {
+                        result.push_back(ans1[i]);
+                        i++;
+                }
+                while(j<ans2.size())
+                {
+                    result.push_back(ans2[j]);
+                        j++;
+                }
+                return;
+        }
 public:
-        
-    void inorder(TreeNode* root, vector<int> &arr){
-       if(root==nullptr)
-       {
-               return;
-       }
-          
-          
-           inorder(root->left, arr);
-             arr.push_back(root->val);
-           inorder(root->right, arr);
-   }
-   
-   void merge(vector<int> arr1, vector<int> arr2, vector<int> &ans){
-       int i=0, j=0;
-       
-       while(i<arr1.size() && j<arr2.size()){
-           if(arr1[i] <arr2[j]){
-               ans.push_back(arr1[i]);
-               i++;
-           } else{
-               ans.push_back(arr2[j]);
-               j++;
-           }
-       }
-       
-       while(i<arr1.size()){
-           ans.push_back(arr1[i]);
-           i++;
-       }
-       
-       while(j<arr2.size()){
-           ans.push_back(arr2[j]);
-           j++;
-       }
-   }
-   
-   
-   
-   vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-       vector<int> arr1, arr2, ans;
-       inorder(root1, arr1);
-       inorder(root2, arr2);
-       merge(arr1, arr2, ans);
-       return ans;
-   }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int>ans1;
+            vector<int>ans2;
+            vector<int>result;
+            inorder(root1,ans1);
+            inorder(root2,ans2);
+          merge(ans1,ans2,result);
+            return result;
+    }
 };
-    
