06b575c18caa40082c962d100018cc3a6f67ce8d
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-04 01:56:35 +0530

    Time: 542 ms (26.78%), Space: 12.1 MB (11.74%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-            vector<int>ans;
-            set<int>result;
-            
-            for(int i=0;i<nums.size();i++)
+
+       
+        set<int>st;  //create set for remove duplicate indexes
+        
+		
+        for(int i=0;i<nums.size();i++)  //traverse the nums
+        {
+            for(int j=0;j<nums.size();j++)
             {
-if(key==nums[i])
-{
-        ans.push_back(i);
-}
-            
+                if(nums[j]==key and abs(i-j)<=k)  //if condition true 
+                    
+                {
+                    st.insert(i);  //insert into set
+                    
+                }
             }
-            
-for(int i=0;i<nums.size();i++)
-            {
-                    for(int j=0;j<ans.size();j++)
-                    {
-                          if(abs(i-ans[j])<=k)
-                          {
-                                 result.insert(i);
-                                  
-                          }
-                         
-                    }                            
-            }
-           
-            vector<int>result2(result.begin(),result.end());
-            return result2;
+        }
+         vector<int>ans(st.begin(),st.end());  //copy elements of set to vector
+        
+        return ans;      //return ans
+        
+        
         
     }
 };
