5d203a78434faf3b05899e3153c0a88c66534f25
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-06 01:10:13 +0530

    Time: 31 ms (64.77%), Space: 22.4 MB (56.77%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int start=0,end=nums.size()-1;
-        int mid=0;
-        
-        while(start<=end){
-             mid=start+(end-start)/2;
-            
-            if(start==end)return nums[start];
-            
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
-                return nums[mid];
-            
-            if(mid%2==1){
-                if(nums[mid]==nums[mid-1])start=mid+1;
-                if(nums[mid]==nums[mid+1])end=mid-1;
-            }
-            
-             if(mid%2==0){
-                if(nums[mid]==nums[mid-1])end=mid-2;
-                if(nums[mid]==nums[mid+1])start=mid+2;
-            }
-            
-        }
-            return -1;
-    }
-};
+    int singleNonDuplicate(vector<int>& nums) 
+     
+{
+int i=0;
+int j=nums.size()-1;
+int ans,mid;
+if(nums.size()==1)
+{
+return nums[0];
+}
+while(i<=j)
+{
+mid=(i+j)/2;
+if(mid%2==0)
+{
+if(nums[mid]==nums[mid+1])
+{
+i=mid+1;
+}else
+{
+j=mid-1;
+}
+}else
+if(mid%2==1)
+{
+if(nums[mid]==nums[mid-1])
+{
+i=mid+1;
+}else
+j=mid-1;
+}
+}
+return nums[i];
+}
+    
+}; 
+  
