d3f37f439168c3687c150dfc4077c9ad129bfb3d
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-06 08:17:43 +0530

    Time: 28 ms (77.55%), Space: 22.4 MB (10.12%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int>memo;
-            int y=0;
+             
+            //first approach : -
+      /*  for(int i=0;i<nums.size()-1;i+=2)
+      {
+           if(nums[i]!=nums[i+1])
+        {
+        return nums[i];
+        }
+        }
+            return -1;
+            */
+            /*
+            unordered_map<int,int>memo;
             for(int i=0;i<nums.size();i++)
             {
                     memo[nums[i]]++;
@@ -11,9 +22,46 @@ public:
             {
                     if(x.second==1)
                     {
-                    y=x.first;        
+                            return x.first;
                     }
+                    }
+            return -1;
+            */
+            
+            int i=0;
+            int j=nums.size()-1;
+            if(nums.size()==1)
+            {
+                    return nums[0];
             }
-            return y;
+            while(i<=j)
+            {
+                    int mid=i+((j-i)/2);
+                    if(mid%2==0)
+                    {
+                            if(nums[mid]==nums[mid+1])
+                            {
+                                    i=mid+1;
+                                    
+                            }
+                            else
+                            {
+                                    j=mid-1;
+                            }
+                    }
+                    if(mid%2==1)
+                    {
+                            if(nums[mid]==nums[mid-1])
+                            {
+                                    i=mid+1;
+                            }
+                            else
+                            {
+                                    j=mid-1;
+                            }
+                    }
+                   
+            }
+             return nums[i];
     }
 };
