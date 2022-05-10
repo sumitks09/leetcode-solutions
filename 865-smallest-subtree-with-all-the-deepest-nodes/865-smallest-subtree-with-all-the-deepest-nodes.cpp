523f22518106fd3f3aa9fcc5776379c7d8cb8f59
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-10 22:22:42 +0530

    Time: 13 ms (15.03%), Space: 14.4 MB (46.57%) - LeetHub

---
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    int height(TreeNode* root)
+    {
+        if(root==nullptr)
+        {
+                return 0;
+        }
+        int lh = height(root->left);
+        int rh = height(root->right);
+        return 1+max(lh,rh);
+    }
+    
+    TreeNode* lca(TreeNode* root,int maxdepth)
+    {
+        if(root==nullptr)
+        {
+                return NULL;
+        }
+            
+        if( maxdepth==1) 
+        {
+                return root;
+        }
+        
+        TreeNode* left = lca(root->left,maxdepth-1);
+        TreeNode* right = lca(root->right,maxdepth-1);
+        
+if(left!=nullptr && right!=nullptr) 
+            return root;
+        else return left ? left : right;
+    }
+    
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        if(root==nullptr) 
+                return NULL;
+        int maxdepth = height(root);
+        return lca(root,maxdepth);
+    }
+};
