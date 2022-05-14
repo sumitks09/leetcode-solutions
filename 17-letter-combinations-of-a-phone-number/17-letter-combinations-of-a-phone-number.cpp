5ec518c5cdb76576399536801258c4181dca7d71
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-14 19:16:32 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (95.84%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,36 +1,37 @@
 class Solution {
         private:
-        void solve(string digits,string mapping[],string ans,int index,vector<string>&output)
+     void phnnumber(string digits,string mapping[],string ans,int index,vector<string>&output)
         {
-            if(index>=digits.size())
-            {
-                    output.push_back(ans);
-                    return;
-            }
-                
-                int number=digits[index]-'0';
-                string result=mapping[number];
-                
-                for(int i=0;i<result.size();i++)
-                {
-                        ans.push_back(result[i]);
-                        solve(digits,mapping,ans,index+1,output);
-                        ans.pop_back();
-                }        
+              if(index>=digits.size())
+              {
+                      output.push_back(ans);
+                      return;
+              }
+             int number=digits[index]-'0';
+             string result=mapping[number];
+             
+             for(int i=0; i<result.size(); i++)
+             {
+                     ans.push_back(result[i]);
+                     phnnumber(digits,mapping,ans,index+1,output);
+                     ans.pop_back();
+             }
+
+             
         }
-        
 public:
     vector<string> letterCombinations(string digits) {
-            string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-            
-        string ans="";
-            
-            vector<string>output;
-            if(digits.size()==0)
+        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+            string ans="";
+             vector<string>output;
+             if(digits.size()==0)
             {
                     return output;
             }
-            solve(digits,mapping,ans,0,output);
+           
+            phnnumber(digits,mapping,ans,0,output);
             return output;
+            
+            
     }
 };
