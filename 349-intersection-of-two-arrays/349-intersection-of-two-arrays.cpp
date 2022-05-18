4feb493e090a4c4df89e38ddec6bbb6bb8ec9d51
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-18 23:56:41 +0530

    Time: 8 ms (57.33%), Space: 10.6 MB (30.68%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-            set<int>s;
-        for(int i=0;i<nums1.size();i++)
-        {
-                for(int j=0;j<nums2.size();j++)
-                {
-                        if(nums1[i]==nums2[j])
-                        {
-                            s.insert (nums1[i]);     
-                        }
-                      
-                }
-        }
+        sort(nums1.begin(),nums1.end());
+             sort(nums2.begin(),nums2.end());
+             vector<int>::iterator ip;
+             vector<int>::iterator ip2;
             
-            vector<int>ans{s.begin(),s.end()};
+             ip = unique(nums1.begin(), nums1.begin() + nums1.size());
+            ip2= unique(nums2.begin(), nums2.begin() + nums2.size());
+            nums1.resize(distance(nums1.begin(),ip));
+            nums2.resize(distance(nums2.begin(),ip2));
+            
+            vector<int>ans;
+                    
+           unordered_map<int,int>memo;
+            for(int i=0;i<nums1.size();i++)
+            {
+                    memo[nums1[i]]++;
+            }
+            for(int i=0;i<nums2.size();i++)
+            {
+                    memo[nums2[i]]++;
+            }
+            
+            for(auto x:memo)
+            {
+                    if(x.second==2)
+                    {
+                        ans.push_back(x.first);    
+                    }
+            }
             return ans;
-            
+                 
     }
 };
