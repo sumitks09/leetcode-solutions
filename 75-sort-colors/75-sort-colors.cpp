62a1c20892a9a83e33756b6d2d10690be4983f77
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-23 20:05:25 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (25.10%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,39 +1,65 @@
 class Solution
 {
+        void mergesorted(vector<int> &nums, int i, int j)
+{
+    int m = i + ((j - i) / 2);
+    int s = i;
+    int e = m + 1;
+
+    vector<int> temp;
+    while (s <= m && e <= j)
+    {
+        if (nums[s] < nums[e])
+        {
+            temp.push_back(nums[s]);
+            s++;
+        }
+        else
+        {
+            temp.push_back(nums[e]);
+            e++;
+        }
+    }
+
+    while (s <= m)
+    {
+        temp.push_back(nums[s]);
+        s++;
+    }
+    while (e <= j)
+    {
+        temp.push_back(nums[e]);
+        e++;
+    }
+
+    int k = 0;
+    for (int idx = i; idx <= j; idx++)
+    {
+        nums[idx] = temp[k++];
+    }
+    return;
+}
+       public:
+        void solve(vector<int> &nums, int i, int j)
+        {
+            if (i >= j)
+            {
+                return;
+            }
+            int m = i + ((j - i) / 2);
+            solve(nums, i, m);
+            solve(nums, m + 1, j);
+
+            return mergesorted(nums, i, j);
+        }
+        
     public:
         void sortColors(vector<int> &nums)
         {
-            int count1 = 0;
-            int count2 = 0;
-            int count3 = 0;
-
-            for (int i = 0; i < nums.size(); i++)
-            {
-                if (nums[i] == 0)
-                {
-                    count1++;
-                }
-                else if (nums[i] == 1)
-                {
-                    count2++;
-                }
-                else
-                {
-                    count3++;
-                }
-            }
-            nums.clear();
-            while (count1--)
-            {
-                nums.push_back(0);
-            }
-            while (count2--)
-            {
-                nums.push_back(1);
-            }
-            while (count3--)
-            {
-                nums.push_back(2);
-            }
+            int i = 0;
+            int j = nums.size() - 1;
+            solve(nums, i, j);
+            
         }
 };
+
