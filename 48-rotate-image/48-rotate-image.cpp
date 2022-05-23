3c6205d0d4028443095199f9374621fffcd02dbf
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-23 14:03:04 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (34.23%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -20,6 +20,7 @@ public:
             }
             */
             
+            /*
             for(int i=0;i<n;i++)
             {
                   for(int j=0;j<i;j++)
@@ -32,6 +33,32 @@ public:
             {
             reverse(matrix[i].begin(),matrix[i].end());
             }
-           
+           */
+            // int n=matrix.size();
+       
+        vector<vector<int>>ans(n,vector<int>(n,0));
+            for(int i=0;i<n;i++)
+            {
+                  for(int j=0;j<n;j++)
+                  {
+                        ans[j][i]=matrix[i][j];
+                          
+                  }
+            }
+             
+            
+              for(int i=0;i<n;i++)
+            {
+                  for(int j=0;j<n;j++)
+                  {
+                        matrix[i][j]=ans[i][j];
+                          
+                  }
+            }
+            for(int i=0;i<n;i++)
+            {
+            reverse(matrix[i].begin(),matrix[i].end());
+            }
+            
     }
 };
