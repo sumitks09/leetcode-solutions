1ca9567ac1dc352a3a301d63e6ef76178dc2c573
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-23 19:52:39 +0530

    Time: 8 ms (9.22%), Space: 8.1 MB (91.00%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,42 +1,39 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-             
-             int count0=0;
-             int count1=0;
-             int count2=0;
-            for(int i=0;i<nums.size();i++)
+class Solution
+{
+    public:
+        void sortColors(vector<int> &nums)
+        {
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+
+            for (int i = 0; i < nums.size(); i++)
             {
-                    if(nums[i]==0)
-                    {
-                            count0++;
-                    }
-                    else if(nums[i]==1)
-                    {
-                            count1++;
-                    }
-                    else
-                    {
-                            count2++;
-                    }
+                if (nums[i] == 0)
+                {
+                    count1++;
+                }
+                else if (nums[i] == 1)
+                {
+                    count2++;
+                }
+                else
+                {
+                    count3++;
+                }
             }
-            
-           nums.clear();
-            while(count0>0)
+            nums.clear();
+            while (count1--)
             {
-                    nums.push_back(0);
-                    count0--;
+                nums.push_back(0);
             }
-            while(count1>0)
+            while (count2--)
             {
-                    nums.push_back(1);
-                    count1--;
+                nums.push_back(1);
             }
-            while(count2>0)
+            while (count3--)
             {
-                    nums.push_back(2);
-                    count2--;
+                nums.push_back(2);
             }
-                
-    }
+        }
 };
