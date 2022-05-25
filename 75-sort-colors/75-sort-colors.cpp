b4d4ec1634e582e780568c781b5dc0cb637ed82b
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-25 15:08:46 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (91.01%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,65 +1,26 @@
-class Solution
-{
-        void mergesorted(vector<int> &nums, int i, int j)
-{
-    int m = i + ((j - i) / 2);
-    int s = i;
-    int e = m + 1;
-
-    vector<int> temp;
-    while (s <= m && e <= j)
-    {
-        if (nums[s] < nums[e])
-        {
-            temp.push_back(nums[s]);
-            s++;
-        }
-        else
-        {
-            temp.push_back(nums[e]);
-            e++;
-        }
-    }
-
-    while (s <= m)
-    {
-        temp.push_back(nums[s]);
-        s++;
-    }
-    while (e <= j)
-    {
-        temp.push_back(nums[e]);
-        e++;
-    }
-
-    int k = 0;
-    for (int idx = i; idx <= j; idx++)
-    {
-        nums[idx] = temp[k++];
-    }
-    return;
-}
-       public:
-        void solve(vector<int> &nums, int i, int j)
-        {
-            if (i >= j)
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+            // dutch national flag 
+            int low=0;
+            int mid=0;
+            int high=nums.size()-1;
+            while(mid<=high)
             {
-                return;
+                    if(nums[mid]==0)
+                    {
+                            swap(nums[low++],nums[mid++]);
+                    }
+                    else if(nums[mid]==1)
+                    {
+                            mid++;
+                    }
+                    else
+                    {
+                            swap(nums[mid],nums[high--]);
+                    }
             }
-            int m = i + ((j - i) / 2);
-            solve(nums, i, m);
-            solve(nums, m + 1, j);
-
-            return mergesorted(nums, i, j);
-        }
-        
-    public:
-        void sortColors(vector<int> &nums)
-        {
-            int i = 0;
-            int j = nums.size() - 1;
-            solve(nums, i, j);
             
-        }
+        
+    }
 };
-
