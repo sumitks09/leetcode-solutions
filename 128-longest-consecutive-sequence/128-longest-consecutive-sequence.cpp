97be277526a3dfe83285f4b8e1271fe596a403cd
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-26 00:11:05 +0530

    Time: 113 ms (66.16%), Space: 33.8 MB (30.09%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-            /*
-        sort(nums.begin(),nums.end());
+            sort(nums.begin(),nums.end());
             int count=1;
-            int n=nums.size();
-            if(n==0)
+            int ans=1;
+            if(nums.size()==0)
             {
                     return 0;
             }
-            else if(n==1)
-            {
-                    return 1;
-            }
-            int largest=1;
             for(int i=1;i<nums.size();i++)
             {
                     if(nums[i]==nums[i-1]+1)
                     {
-                            count++;
-                            largest=max(largest,count);
+                           count++; 
                     }
                     else if(nums[i]==nums[i-1])
                     {
@@ -27,47 +20,12 @@ public:
                     }
                     else
                     {
-                            
                             count=1;
                     }
+                    ans=max(ans,count);
+            }
+            return ans;
                     
-            }
-            return largest;
-            */
-            
-            int mx=1;
-            
-            unordered_set<int>s;
-            for(int i=0;i<nums.size();i++)
-            {
-                    s.insert(nums[i]);
-            }
-            if(s.size()==0)
-            {
-                    return 0;
-            }
-            
-            
-            for(auto i:s)
-            {
-                    if(s.find(i-1)==s.end())
-                    {
-                            int len=1;
-                            int curr=i;
-                            while(s.find(curr+1)!=s.end())
-                            {
-                                    curr=curr+1;
-                                    len++;
-                            }
-                            mx=max(mx,len);
-                    }
-                    
-            }
-            return mx;
-            
-            
-            
-     
-            
+        
     }
 };
