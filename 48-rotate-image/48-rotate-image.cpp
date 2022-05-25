11aa5fda6650ba87a0c2283ebb42182b1019a19d
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-25 20:57:41 +0530

    Time: 3 ms (58.91%), Space: 7.1 MB (73.72%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,64 +1,37 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-            int n=matrix.size();
-            /*
-        vector<vector<int>>ans(n,vector<int>(n,0));
-            for(int i=0;i<n;i++)
+int n=matrix.size();
+            vector<vector<int>>mat(n,vector<int>(n,0));
+            for(int i=0;i<matrix.size();i++)
             {
-                    for(int j=0;j<n;j++)
+                    for(int j=0;j<matrix[0].size();j++)
                     {
-                            ans[j][n-i-1]=matrix[i][j];
-                    }
+                            mat[i][j]=matrix[j][i];
+                            
+                            }
+                            
             }
+            
             for(int i=0;i<n;i++)
             {
-                    for(int j=0;j<n;j++)
+                    reverse(matrix[i].begin(),matrix[i].end());
+            }
+            for(int i=0;i<matrix.size();i++)
+            {
+                    for(int j=0;j<matrix[0].size();j++)
                     {
-                            matrix[i][j]=ans[i][j];
-                    }
+                            matrix[i][j]=mat[i][j];
+                            
+                            }
+                            
             }
-            */
-            
-            /*
-            for(int i=0;i<n;i++)
+             for(int i=0;i<n;i++)
             {
-                  for(int j=0;j<i;j++)
-                  {
-                         
-                          swap(matrix[i][j], matrix[j][i]);
-                  }
-            }
-            for(int i=0;i<n;i++)
-            {
-            reverse(matrix[i].begin(),matrix[i].end());
-            }
-           */
-            // int n=matrix.size();
-       
-        vector<vector<int>>ans(n,vector<int>(n,0));
-            for(int i=0;i<n;i++)
-            {
-                  for(int j=0;j<n;j++)
-                  {
-                        ans[j][i]=matrix[i][j];
-                          
-                  }
-            }
-             
-            
-              for(int i=0;i<n;i++)
-            {
-                  for(int j=0;j<n;j++)
-                  {
-                        matrix[i][j]=ans[i][j];
-                          
-                  }
-            }
-            for(int i=0;i<n;i++)
-            {
-            reverse(matrix[i].begin(),matrix[i].end());
+                    reverse(matrix[i].begin(),matrix[i].end());
             }
             
+                    
+        
     }
 };
