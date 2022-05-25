118327ea12e1388b1b5c3ce47916113f4d88948f
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-25 23:51:25 +0530

    Time: 177 ms (50.17%), Space: 49.8 MB (16.10%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -67,25 +67,7 @@ public:
             
             
             
-      /*
-        int mx=1;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(s.find(nums[i]-1)==s.end())
-            {
-                int len=1;
-                int curr=nums[i];
-                while(s.find(curr+1)!=s.end())
-                {
-                    curr=curr+1;
-                    len++;
-                }
-                mx=max(mx,len);
-            }
-        }
-        return mx;
-    }
-          */  
+     
             
     }
 };
