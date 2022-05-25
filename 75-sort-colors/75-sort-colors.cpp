6e926a3f590eedb91becbbcb943ff643dfde6ae7
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-25 15:14:09 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (71.17%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
             // dutch national flag 
+            /*
             int low=0;
             int mid=0;
             int high=nums.size()-1;
@@ -20,7 +21,39 @@ public:
                             swap(nums[mid],nums[high--]);
                     }
             }
-            
+            */
+            // counting sort
+            int count1=0;
+            int count2=0;
+            int count3=0;
+            for(int i=0;i<nums.size();i++)
+            {
+                    if(nums[i]==0)
+                    {
+                            count1++;
+                    }
+                    else if(nums[i]==1){
+                            count2++;
+                    }
+                    else
+                    {
+                            count3++;
+                    }
+                    
+            }
+            nums.clear();
+            while(count1--)
+            {
+                    nums.emplace_back(0);
+            }
+            while(count2--)
+            {
+                    nums.emplace_back(1);
+            }
+            while(count3--)
+            {
+                    nums.emplace_back(2);
+            }
         
     }
 };
