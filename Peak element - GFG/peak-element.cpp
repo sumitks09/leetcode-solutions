c57f6ff0a914296a28710bb9f95a228d321f32c2
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-05-26 13:13:35 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.cpp
+++ Peak element - GFG/peak-element.cpp
@@ -14,17 +14,20 @@ class Solution
     public:
     int peakElement(int arr[], int n)
     {
+        if(n==1)
+        {
+            return 0;
+        }
         
-        
-       for(int i=0;i<n-1;i++)
+       for(int i=1;i<n;i++)
        {
            if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])
            {
                return i;
            }
-           if(arr[i]>arr[i+1]&&i==0)
+           if(arr[i]>arr[i+1]&&i==1)
            {
-               return i;
+               return i-1;
            }
             else if(arr[i]>arr[i-1]&&i==n-1)
            {
