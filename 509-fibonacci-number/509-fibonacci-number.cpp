03b211104773c7eea40f018819ba7bba99d28b86
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-02 00:22:19 +0530

    Time: 11 ms (37.69%), Space: 5.9 MB (80.45%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)
+        {
+                return n;
+        }
+            
+            int a=fib(n-1);
+            int b=fib(n-2);
+            return a+b;
+            
+    }
+};
