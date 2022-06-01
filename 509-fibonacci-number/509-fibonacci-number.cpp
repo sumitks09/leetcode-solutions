0b45b47dcefdcd91b57aa515d4e1f1619fffa3ae
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-02 00:28:31 +0530

    Time: 5 ms (43.45%), Space: 6.3 MB (10.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,29 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n<=1)
+class Solution
+{
+    private:
+        int helper(int n, unordered_map<int, int> &memo)
         {
+            if (n <= 1)
+            {
                 return n;
+            }
+
+            int current_key = n;
+            if (memo.find(current_key) != memo.end())
+            {
+                return memo[current_key];
+            }
+            int a = helper(n - 1, memo);
+            int b = helper(n - 2, memo);
+
+            memo[current_key] = a + b;
+            return memo[current_key];
         }
-            
-            int a=fib(n-1);
-            int b=fib(n-2);
-            return a+b;
-            
-    }
+    public:
+        int fib(int n)
+        {
+
+            unordered_map<int, int> memo;;
+            return helper(n, memo);
+        }
 };
