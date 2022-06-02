dcb5bf24e72f9d108ff4c0ce5bd469a549ebb1b6
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-02 13:51:51 +0530

    Time: 7 ms (14.41%), Space: 6.9 MB (99.75%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,37 +1,27 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-int n=matrix.size();
-            vector<vector<int>>mat(n,vector<int>(n,0));
-            for(int i=0;i<matrix.size();i++)
+class Solution
+{
+    public:
+        void rotate(vector<vector < int>> &matrix)
+        {
+            for (int i = 0; i < matrix.size(); i++)
             {
-                    for(int j=0;j<matrix[0].size();j++)
-                    {
-                            mat[i][j]=matrix[j][i];
-                            
-                            }
-                            
+                for (int j = 0; j < i; j++)
+                {
+                   swap (matrix[j][i], matrix[i][j]);
+                }
             }
-            
-            for(int i=0;i<n;i++)
-            {
-                    reverse(matrix[i].begin(),matrix[i].end());
-            }
-            for(int i=0;i<matrix.size();i++)
-            {
-                    for(int j=0;j<matrix[0].size();j++)
-                    {
-                            matrix[i][j]=mat[i][j];
-                            
-                            }
-                            
-            }
-             for(int i=0;i<n;i++)
-            {
-                    reverse(matrix[i].begin(),matrix[i].end());
-            }
-            
-                    
-        
-    }
+                
+                
+                for(int i=0;i<matrix.size();i++)
+                {
+                        reverse(matrix[i].begin(),matrix[i].end());
+                }
+                
+                
+                
+                
+                
+                
+                
+        }
 };
