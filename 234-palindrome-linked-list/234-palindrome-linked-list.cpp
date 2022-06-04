1d11bb10d44ab27d6595dfd7796e4b5be2863902
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-04 22:30:57 +0530

    Time: 230 ms (91.08%), Space: 114.1 MB (85.19%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,80 +1,60 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *int val;
+ *ListNode * next;
+ *ListNode() : val(0), next(nullptr) {}
+ *ListNode(int x) : val(x), next(nullptr) {}
+ *ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-        private:
-        ListNode* reverselist(ListNode* head)
+class Solution
+{
+    public:
+        ListNode* reverselist(ListNode *head)
         {
-                 if(head==nullptr)
+            if (head == nullptr)
+            {
+                return head;
+            }
+            ListNode *prev = nullptr;
+            ListNode *curr = head;
+            ListNode *n = curr->next;
+            while (curr != nullptr)
+            {
+                curr->next = prev;
+                prev = curr;
+                curr = n;
+                if (n != nullptr)
                 {
-                        return head;
+                    n = n->next;
                 }
-            ListNode* prev=nullptr;
-                ListNode* curr=head;
-                ListNode* n=curr->next;
-                while(curr!=nullptr)
-                {
-                        curr->next=prev;
-                        prev=curr;
-                        curr=n;
-                        if(n!=nullptr)
-                        {
-                                n=n->next;
-                        }
-                }
-                return prev;
+            }
+            return prev;
         }
-        ListNode* midpoint(ListNode* head)
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr)
         {
-            ListNode* fast=head;
-            ListNode* slow=head;
-                
-                while(fast->next!=nullptr && fast->next->next!=nullptr)
-                {
-                     fast=fast->next->next;
-                        slow=slow->next;
-                }
-                if(fast->next!=nullptr)
-                {
-                        return slow->next;
-                }
-                return slow;
-                
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        
-public:
-    bool isPalindrome(ListNode* head) {
-             if(head==nullptr)
-                {
-                        return head;
-                }
-            ListNode* temp2=head;
-       ListNode* curr= midpoint(temp2);
-            ListNode* temp=reverselist(curr);
-            
-            bool flag=true;
-            while(temp!=nullptr)
+        ListNode *midp = slow->next;
+        ListNode *mid = reverselist(midp);
+        while (mid != nullptr)
+        {
+            if (head->val != mid->val)
             {
-            if(temp->val!=head->val)
-            {
-                    flag=false;
-                    return flag;
+                return false;
             }
             else
             {
-                    temp=temp->next;
-                    head=head->next;
+                head = head->next;
+                mid = mid->next;
             }
-            }
-            return flag;
-            
-            
+        }
+        return true;
     }
 };
