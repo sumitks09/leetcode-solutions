cefe7f33bff327dc67fdb178be1d276642fc59fb
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-05 23:06:10 +0530

    Time: 48 ms (86.10%), Space: 14.7 MB (31.53%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -1,37 +1,55 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *   int val;
+ *   ListNode * next;
+ *   ListNode(int x) : val(x), next(NULL) {}
+ *};
  */
-class Solution {
-public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *temp=headA;
-            ListNode *temp2=headB;
-            while(temp!=temp2)
+class Solution
+{
+    public:
+        ListNode* getIntersectionNode(ListNode *headA, ListNode *headB)
+        {
+            if (headA == nullptr)
             {
-            if(temp==nullptr)
-            {
-                    temp=headB;
+                return nullptr;
             }
-            else
+            int size1 = 0;
+            int size2 = 0;
+            ListNode *temp = headA;
+            ListNode *temp2 = headB;
+            while (temp != nullptr)
             {
-                    temp=temp->next;
+                size1++;
+                temp = temp->next;
             }
-            if(temp2==nullptr)
+            while (temp2 != nullptr)
             {
-                    temp2=headA;
+                size2++;
+                temp2 = temp2->next;
+            }
+
+            int plus = abs(size1 - size2);
+
+                while(plus--)
+                {
+            if (size1 < size2)
+            {
+                headB = headB->next;
                     
             }
             else
             {
-                    temp2=temp2->next;
+                headA = headA->next;
             }
+                }
+
+            while (headA != headB)
+            {
+                headA = headA->next;
+                headB = headB->next;
             }
-            return temp;
-            
-    }
+            return headB;
+        }
 };
