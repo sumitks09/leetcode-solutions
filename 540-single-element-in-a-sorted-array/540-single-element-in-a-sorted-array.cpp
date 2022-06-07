6ca5070a69a398d6d31b792a0324ab7069066204
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-07 14:13:41 +0530

    Time: 66 ms (5.21%), Space: 27.4 MB (9.13%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -4,6 +4,7 @@ class Solution
         int singleNonDuplicate(vector<int> &nums)
         {
             int n = nums.size();
+                /*
             for (int i = 0; i < n - 1; i+=2)
             {
                 if (nums[i] != nums[i + 1])
@@ -13,7 +14,26 @@ class Solution
             }
             return nums[nums.size()-1];
         }
+        */
+                
+                
+                unordered_map<int,int>memo;
+                for(int i=0;i<n;i++)
+                {
+                        memo[nums[i]]++;
+                }
+                
+                for(auto x:memo)
+                {
+                        if(x.second==1)
+                        {
+                                return x.first;
+                        }
+                }
+                return -1;
         
+        
+        }
         
         
         
