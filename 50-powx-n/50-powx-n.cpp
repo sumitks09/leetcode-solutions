d3239349a4da1fdc8a48106469880ef98302c15e
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-07 21:17:23 +0530

    Time: 3 ms (43.85%), Space: 6.2 MB (13.09%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+            /*
+            double result=1.0;
+            for(int i=0;i<n;i++)
+            {
+                   result=result*x;
+            }
+            return result;
+            */
+            
+            
+            return pow(x,n);
+        
+    }
+};
