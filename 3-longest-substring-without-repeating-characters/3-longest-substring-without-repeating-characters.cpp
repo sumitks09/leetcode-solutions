caeb0d1e623ad3e0637db687fcf97f50d5ed2f6c
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-10 08:22:15 +0530

    Time: 1910 ms (5.00%), Space: 243.8 MB (5.40%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -14,30 +14,12 @@ class Solution
                     if (set.find(s[j]) != set.end())
                     {
 
-                        
                         break;
                     }
                     set.insert(s[j]);
-                        ans = max(ans, (int)set.size());
+                    ans = max(ans, (int) set.size());
                 }
             }
             return ans;
         }
 };
-/*
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int ans = 0;
-        for (int i = 0; i < s.length(); i++) {
-            unordered_set<char> st;
-            for(int j = i; j < s.length(); j++) {
-                if(st.find(s[j]) != st.end()) {
-                    break;
-                }
-                st.insert(s[j]);
-                ans = std::max(ans, (int)st.size());
-            }
-        }
-        return ans;*/
-   
