1c85919309b06b3ef8cba043f5e1969761ffd020
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-06-15 14:27:00 +0530

    Time: 1029 ms (46.44%), Space: 300.4 MB (5.02%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,61 @@
+/**
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *int val;
+ *ListNode * next;
+ *ListNode() : val(0), next(nullptr) {}
+ *ListNode(int x) : val(x), next(nullptr) {}
+ *ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
+ */
+class Solution
+{
+    private:
+        void insert(ListNode **root, int item)
+        {
+           ListNode* temp = new ListNode;
+            temp->val = item;
+            temp->next = *root;
+            *root = temp;
+        }
+    private:
+        ListNode* arrayToList(vector<int> &nums, int n)
+        {
+            ListNode *root = NULL;
+            for (int i = n - 1; i >= 0; i--)
+                insert(&root, nums[i]);
+            return root;
+        }
+
+    public:
+        ListNode* mergeNodes(ListNode *head)
+        {
+            vector<int> ans;
+            vector<int> nums;
+            ListNode *temp = head;
+            while (temp != nullptr)
+            {
+                ans.push_back(temp->val);
+                temp = temp->next;
+            }
+
+            int sum = 0;
+            int i = 0;
+            int j = i + 1;
+            while (i != ans.size() - 1)
+            {
+                while (ans[j] != 0)
+                {
+                    sum += ans[j];
+                    j++;
+                }
+                nums.push_back(sum);
+                sum = 0;
+                i = j;
+                j = i + 1;
+            }
+            int n = nums.size();
+            ListNode *curr = arrayToList(nums, n);
+            return curr;
+        }
+};
