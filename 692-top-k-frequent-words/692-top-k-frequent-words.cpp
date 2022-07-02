9c39b92509dead3b0124907ab4c7b389fd38979b
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-02 16:24:26 +0530

    Time: 20 ms (49.64%), Space: 12.8 MB (33.74%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,39 +1,55 @@
 class Solution {
 public:
-    static bool cmp(pair<string,int>&a,pair<string,int>&b)
+    static bool cmp(pair<string,int>a,pair<string,int>b)
     {
         if(a.second>b.second)
+        {
             return true;
-        //if the count of two strings is equal, then check for the lexographically smaller string
-        if(a.second==b.second)
+        }
+        else if(a.second==b.second)
         {
             if(a.first<b.first)
+            {
                 return true;
+            }
+        }
+        else
+        {
+            return false;
         }
         return false;
+        
     }
     vector<string> topKFrequent(vector<string>& words, int k) {
-       unordered_map<string,int>m;
-        for(auto i: words)
+        
+        unordered_map<string,int>memo;
+        for(int i=0;i<words.size();i++)
         {
-            m[i]++;
+            memo[words[i]]++;
         }
-        vector<pair<string,int>>v;
-        for(auto it:m)
+        
+        vector<pair<string,int>>vec;
+        
+        for(auto it=memo.begin();it!=memo.end();it++)
         {
-            v.push_back({it.first,it.second});
+            vec.push_back(make_pair(it->first,it->second));
         }
-        sort(v.begin(),v.end(),cmp);
-        vector<string> ans;
-        for(auto x:v)
+        
+        sort(vec.begin(),vec.end(),cmp);
+        
+        vector<string>result;
+        for(auto x:vec)
         {
             if(k>0)
             {
-                ans.push_back(x.first);
-                k--;
+                result.push_back(x.first);
+                
             }
+            k--;
         }
+        return result;
         
-        return ans;
+        
+        
     }
 };
