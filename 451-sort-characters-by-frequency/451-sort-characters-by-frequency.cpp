8bf76538e18cf445e0c38c76113c1f32cc303290
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-03 20:22:21 +0530

    Time: 15 ms (67.89%), Space: 8.4 MB (65.30%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,33 +1,36 @@
-class Solution {
-public:
-    static bool cmp(pair<char,int>a,pair<char,int>b)
+class Solution
+{
+    public:
+        static bool cmp(pair<char, int> a, pair<char, int> b)
+        {
+            return a.second > b.second;
+
+            return false;
+        }
+    string frequencySort(string s)
     {
-       return a.second>b.second;
-       
-       
-          return false;
-        
-    }
-    string frequencySort(string s) {
-        unordered_map<char,int>memo;
-        for(int i=0;i<s.size();i++)
+        unordered_map<char, int> memo;
+        for (int i = 0; i < s.size(); i++)
         {
             memo[s[i]]++;
         }
-        
-        vector<pair<char,int>>vec;
-        
-        for(auto it=memo.begin();it!=memo.end();it++)
+
+        vector<pair<char, int>> vec;
+
+        for (auto it = memo.begin(); it != memo.end(); it++)
         {
-            vec.push_back(make_pair(it->first,it->second));
+            vec.push_back(make_pair(it->first, it->second));
         }
+
+        sort(vec.begin(), vec.end(), cmp);
+
         
-        sort(vec.begin(),vec.end(),cmp);
-        
-        string str;
-        for(int i = 0; i < vec.size(); i++) {
-            for(int j = 0; j < vec[i].second; j++) {
-                str += vec[i].first;
+        string str="";
+        for(auto y:vec)
+        {
+            while(y.second--)
+            {
+            str.push_back(y.first);
             }
         }
         return str;
