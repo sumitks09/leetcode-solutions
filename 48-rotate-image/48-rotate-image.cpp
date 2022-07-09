695c9ebbbee70e4cd8d82621dd20f4d0bbd0a94b
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-09 23:26:16 +0530

    Time: 3 ms (63.56%), Space: 7.1 MB (33.38%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         for(int i=0;i<matrix.size();i++)
         {
-            for(int j=0;j<matrix[0].size();j++)
+            for(int j=0;j<matrix.size();j++)
             {
                 if(i<j)
                 {
