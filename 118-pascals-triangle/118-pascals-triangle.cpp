bff2621a0fa971633c0bfd7c509c55d848cace5f
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-19 23:49:23 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,23 +1,23 @@
-class Solution
-{
-    public:
-        vector<vector < int>> generate(int numRows)
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>vec(numRows);
+        for(int i=0;i<numRows;i++)
         {
-            vector<vector < int>> vec(numRows);
-            for (int i = 0; i < numRows; i++)
+            for(int j=0;j<=i;j++)
             {
-                for (int j = 0; j <= i; j++)
+                if(j==0 || i==j)
                 {
-                    if(i==j || j==0)
-                    {
-                            vec[i].push_back(1);
-                    }
-                    else
-                    {
-                            vec[i].push_back(vec[i-1][j]+vec[i-1][j-1]);
-                    }
+                    vec[i].push_back(1);
+                }
+                else
+                {
+                    vec[i].push_back(vec[i-1][j-1]+vec[i-1][j]);
                 }
             }
-            return vec;
+                
         }
+        return vec;
+        
+    }
 };
