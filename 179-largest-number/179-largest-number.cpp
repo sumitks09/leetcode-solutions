0b7c64af0340913d8605d85f0ce8692de8027e32
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-25 13:05:57 +0530

    Time: 13 ms (58.83%), Space: 11.6 MB (11.26%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -1,31 +1,28 @@
-class Solution
-{
+class Solution {
     private:
-        static bool cmp(const string &s1, const string &s2)
+    static bool cmp(string &s1,string &s2)
+    {
+        return ((s1+s2)>(s2+s1));
+    }
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string>vec;
+        
+        for(int i=0;i<nums.size();++i)
         {
-            return ((s1 + s2) > (s2 + s1));
+            vec.push_back(to_string(nums[i]));
         }
-
-    public:
-        string largestNumber(vector<int> &nums)
+        
+        sort(vec.begin(),vec.end(),cmp);
+        string temp="";
+        for(int i=0;i<vec.size();i++)
         {
-            int maxi=*max_element(nums.begin(),nums.end());
-            
-            if(maxi==0)
-            {
-                return "0";
-            }
-            vector<string> ans;
-            for (int i = 0; i < nums.size(); i++)
-            {
-                ans.push_back(to_string(nums[i]));
-            }
-        sort(ans.begin(),ans.end(),cmp);
-            string temp = "";
-            for (int i = 0; i < ans.size(); i++)
-            {
-                temp += ans[i];
-            }
-            return temp;
+            temp+=vec[i];
         }
+        if(vec[0]=="0")
+        {
+            return "0";
+        }
+        return temp;
+    }
 };
