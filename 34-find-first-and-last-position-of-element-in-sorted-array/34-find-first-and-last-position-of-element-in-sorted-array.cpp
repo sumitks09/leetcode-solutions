7ba213be093d2b9a1c5a0b55f1c686eb281e9473
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-25 18:42:00 +0530

    Time: 10 ms (73.39%), Space: 13.8 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,16 +1,60 @@
-class Solution
-{
-    public:
-        vector<int> searchRange(vector<int> &nums, int target)
+class Solution {
+    private:
+    int firstpos(vector<int>&nums,int target)
+    {
+        int i=0;
+        int j=nums.size()-1;
+        int result=-1;
+        while(i<=j)
         {
-        auto low = lower_bound(nums.begin(),nums.end(),target);
-        auto  up = upper_bound(nums.begin(),nums.end(),target);
-        
-        if(!binary_search(nums.begin(),nums.end(),target))    return {-1,-1};
-        
-        int first = low- nums.begin();
-        int last = up - nums.begin()-1;
-        
-        return {first ,last};
+            int mid=(i+(j-i)/2);
+            if(nums[mid]==target)
+            {
+                result=mid;
+                j=mid-1;
+            }
+            else if(nums[mid]>target)
+            {
+                j=mid-1;
+            }
+            else
+            {
+                i=mid+1;
+            }
         }
+        return result;
+    }
+    private:
+    int lastpos(vector<int>&nums,int target)
+    {
+        int i=0;
+        int j=nums.size()-1;
+        int result=-1;
+        while(i<=j)
+        {
+            int mid=(i+(j-i)/2);
+            if(nums[mid]==target)
+            {
+                result=mid;
+                i=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+               j=mid-1;
+            }
+            else
+            {
+                i=mid+1;
+            }
+        }
+        return result;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>ans(2,0);
+        int n=nums.size();
+        ans[0]=firstpos(nums,target);
+        ans[1]=lastpos(nums,target);
+        return ans;
+    }
 };
