9e6e848ed23b0f4610a69970c4593fc989eec6c3
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-26 19:56:15 +0530

    Time: 44 ms (78.29%), Space: 17.2 MB (81.73%) - LeetHub

---
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -0,0 +1,18 @@
+#define ll long long
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        int n=nums.size();
+	vector<pair<long,int>> vec;//vector to store <value,index> pair
+	for(int i=0;i<n;i++) vec.push_back({nums[i],i});
+	sort(vec.begin(),vec.end());//sort based on values
+	for(int i=0;i<n;i++){
+		//iterate from i+1th index to some j such that differnce of values (vec[j]-vec[i])<=t  
+		for(int j=i+1;j<n and (vec[j].first*1L-vec[i].first)<=t;j++){
+			//if the absolute difference of indices <=k then required pair is found 
+			if(abs(vec[j].second-vec[i].second)<=k) return true;
+		}
+	}
+	return false;
+    }
+};
