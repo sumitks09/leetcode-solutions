67797ab534103fd3740112556ed9423e3b3d305e
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-26 19:57:38 +0530

    Time: 28 ms (94.31%), Space: 17 MB (90.51%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -8,7 +8,7 @@ public:
 	sort(vec.begin(),vec.end());//sort based on values
 	for(int i=0;i<n;i++){
 		//iterate from i+1th index to some j such that differnce of values (vec[j]-vec[i])<=t  
-		for(int j=i+1;j<n and (vec[j].first*1L-vec[i].first)<=t;j++){
+		for(int j=i+1;j<n and (vec[j].first*1LL-vec[i].first)<=t;j++){
 			//if the absolute difference of indices <=k then required pair is found 
 			if(abs(vec[j].second-vec[i].second)<=k) return true;
 		}
