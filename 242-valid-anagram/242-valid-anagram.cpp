4484f3e13c89aa88bfd63298dd5d98786add3142
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-28 15:12:15 +0530

    Time: 5 ms (92.06%), Space: 7.4 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,29 @@
+#define ucmap unordered_map<char,int>
+#define pb push_back
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
+        ucmap memo;
+       
+        
+        for(const auto &x:s)
+        {
+            memo[x]++;
+        }
+        for(const auto &y:t)
+        {
+            memo[y]--;
+        }
+        for(auto z:memo)
+        {
+          if(z.second>0||z.second<0)
+          return false;
+        }
+        return true;
+        
+    }
+};
