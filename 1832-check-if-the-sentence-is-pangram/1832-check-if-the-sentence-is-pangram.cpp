ab00589145bd7076fb84c3edd9c74f1cc10d279d
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-29 22:46:07 +0530

    Time: 4 ms (56.20%), Space: 6.7 MB (26.63%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,24 @@
+#define ucmap unordered_map<char,int>memo
+#define pb push_back
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
+        int count=0;
+        ucmap;
+        
+        for(auto x:sentence) memo[x]++;
+        
+        for(auto i:memo)
+        {   
+            if(i.second>0) count++;
+         }
+        if(count==26) return true;
+        return false;
+
+    }
+};
