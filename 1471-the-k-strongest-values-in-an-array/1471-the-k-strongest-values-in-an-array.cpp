75e6e2dc4279dd12ba4aec4adeabcc81b25396f9
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-30 15:43:05 +0530

    Time: 567 ms (25.89%), Space: 97.3 MB (7.52%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
@@ -2,8 +2,10 @@ class Solution {
     private:
     static bool cmp(const pair<int,int>&a , const pair<int,int>&b)
     {
-        if(a.second==b.second) return a.first>b.first;
-        return a.second>b.second;
+         if(a.second==b.second) return a.first>b.first;
+         return a.second>b.second;
+       
+       
     }
         
 public:
@@ -12,7 +14,7 @@ public:
         vector<pair<int,int>>pq;
         sort(arr.begin(),arr.end());
         
-         int median = arr[(arr.size() - 1)/ 2] ;
+        int median=arr[((n-1)/2)];
         for(int i=0;i<n;i++)
         {
             pq.push_back({arr[i],abs(arr[i]-median)});
@@ -21,9 +23,7 @@ public:
         vector<int>result;
         
         for(int i=0;i<k;i++)
-        {
             result.push_back(pq[i].first);
-        }
         return result;
             
     }
