d42fa2b6e61fb000350c3ae59dbb1fefd74ef669
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-08-01 01:44:39 +0530

    Time: 14 ms (80.75%), Space: 21.9 MB (6.49%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,47 +1,46 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-        private:
-        void solve(TreeNode* root,vector<int>&ans)
+class Solution
+{
+    void helper(TreeNode *root, vector<int> &ans)
+    {
+        if (root == nullptr)
         {
-        if(root==nullptr)
-        {
-                return;
+            return;
         }
-                solve(root->left,ans);
-                ans.push_back(root->val);
-                solve(root->right,ans);
-       
-        }
-        
-        bool issorted(vector<int>&ans,int n)
-        {
-                
-                for(int i=0;i<n-1;i++)
-                {
-                    if(ans[i]>=ans[i+1])
-                    {
-                            return false;
-                    }
-                }
-                return true;
-        }
-public:
-    bool isValidBST(TreeNode* root) {
-            vector<int>ans;
-            solve(root,ans);
-            int n=ans.size();
-            bool result=issorted(ans,n);
-            return result;
-        
+        helper(root->left,ans);
+        ans.push_back(root->val);
+        helper(root->right,ans);
     }
+
+    bool issorted(vector<int> &ans,int n)
+    {
+        for (int i = 0; i < n-1; i++)
+        {
+            if (ans[i] >= ans[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public:
+        bool isValidBST(TreeNode *root)
+        {
+            vector<int> ans;
+            helper(root, ans);
+            int n=ans.size();
+            return issorted(ans,n);
+            
+        }
 };
