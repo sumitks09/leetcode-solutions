c4edbe694372818333a2694609cafe1240f9d9ed
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-31 13:30:21 +0530

    Time: 13 ms (55.62%), Space: 11.2 MB (73.22%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,40 +1,42 @@
-#define uimap unordered_map<int,int>memo
-#define pb push_back
-#define vpii vector<pair<int,int>>
-#define vi vector<int>
-class Solution {
-    
-    private:
-    static bool cmp(pair<int,int>&x1,pair<int,int>&x2)
-    {
-       if(x1.second==x2.second) return x1.first>x2.first;
-        return x1.second<x2.second;
-    }
-public:
-    vector<int> frequencySort(vector<int>& nums) {
-        uimap;
-        vpii pq;
-       // vi result;
-        for(const auto &x:nums)
-        {
-            memo[x]++;
-        }
-        
-        for(auto it=memo.begin();it!=memo.end();it++)
-            pq.pb({it->first,it->second});
-        
-        sort(pq.begin(),pq.end(),cmp);
-       nums.clear();
-        for(int i=0;i<pq.size();i++)
-        {
-            while(pq[i].second--)
-            {
-           nums.pb(pq[i].first);
-                
-           }
-        }
-        return nums;
 
-        
-    }
-};
+#define uimap unordered_map<int, int> memo
+#define pb push_back
+#define vpii vector<pair<int, int>>
+#define vi vector < int>
+#define mapitr for(auto it = memo.begin(); it != memo.end(); it++)
+    class Solution
+    {
+
+        private:
+            static bool cmp(pair<int, int> &x1, pair<int, int> &x2)
+            {
+                if (x1.second == x2.second) return x1.first > x2.first;
+                return x1.second < x2.second;
+            }
+        public:
+            vector<int> frequencySort(vector<int> &nums)
+            {
+                uimap;
+                vpii pq;
+
+                for (const auto &x: nums)
+                {
+                    memo[x]++;
+                }
+
+                mapitr
+                    pq.pb({ it->first,
+                        it->second });
+
+                sort(pq.begin(), pq.end(), cmp);
+                nums.clear();
+                for (int i = 0; i < pq.size(); i++)
+                {
+                    while (pq[i].second--)
+                    {
+                        nums.pb(pq[i].first);
+                    }
+                }
+                return nums;
+            }
+    };
