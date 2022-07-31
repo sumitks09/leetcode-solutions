f01de5f3891ff9cf3507ed3a669255c544f3fb4f
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-07-31 23:51:44 +0530

    Time: 648 ms (14.55%), Space: 153.3 MB (17.97%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,37 +1,39 @@
-class Solution
-{
+#define pb push_back
+#define dvi vector<vector<int>>
+#define vi vector<int>
+#define frn for(int i=1;i<=n;i++)
+
+class Solution {
     private:
-       void subsets(vector<int> &x, int n, int k,int index ,vector<vector< int>> &ans,vector<int>&temp,int size)
+    void subsets(int n,vector<int>&vec,vector<vector<int>>&result,int current,vector<int>&nums)
+    {
+        if(current>=n)
         {
-            if (index >= size)
+            if(vec.size()>=1)
             {
-                if (temp.size() == k)
-                {
-                    ans.push_back(temp);
-                }
-                return;
+            result.push_back(vec);
             }
-
-           	//include
-            temp.push_back(x[index]);
-            subsets(x, n, k, index + 1, ans, temp,size);
-           	//exclude
-            temp.pop_back();
-            subsets(x, n, k, index + 1, ans, temp,size);
+            return;
         }
-
-    public:
-        vector<vector < int>> combine(int n, int k)
-        {
-            vector<int> x;
-            for (int i = 1; i <= n; i++)
-            {
-                x.push_back(i);
-            }
-                int size=x.size();
-            vector<vector < int>> ans;
-            vector<int> temp;
-            subsets(x, n, k, 0, ans, temp,size);
-           return ans;
-        }
+        
+        vec.pb(nums[current]);
+        subsets(n,vec,result,current+1,nums);
+        
+        vec.pop_back();
+        subsets(n,vec,result,current+1,nums);
+        
+        
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        dvi result;
+        vi vec;
+        vi nums;
+        dvi result2;
+        frn nums.push_back(i);
+        subsets( n,vec,result,0,nums);
+        for(const auto &x:result) if(x.size()==k) result2.push_back(x);
+        return result2;
+            
+    }
 };
