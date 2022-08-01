cc29bb98634bd638a5b586a047bc7e6957698607
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-08-01 22:40:43 +0530

    Time: 22 ms (39.69%), Space: 11.1 MB (74.91%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,32 +1,30 @@
 class Solution {
-        void helper(vector<int>& candidates, vector<vector<int>>&ans, vector<int>&temp,int index,int target,int n)
+    private:
+    void helper(vector<int>& candidates,int target,int index,vector<vector<int>>&result,vector<int>&vec)
+    {
+        if(index==candidates.size())
         {
-                if(index==n)
-                {
-                   if(target==0)
-                   {
-                           ans.push_back(temp);
-                   }
-                        return;
-                }
-                
-                if(candidates[index]<=target)
-                {
-                        temp.push_back(candidates[index]);
-                                helper(candidates,ans,temp,index,target-candidates[index],n);
-                        temp.pop_back();
-                }
-                helper(candidates,ans,temp,index+1,target,n);
-                
-                
+           if(target==0)
+           {
+               result.push_back(vec);
+           }
+            return;
         }
+        
+        if(candidates[index]<=target)
+        {
+            vec.push_back(candidates[index]);
+            helper(candidates,target-candidates[index],index,result,vec);
+            vec.pop_back();
+        }
+         helper(candidates,target,index+1,result,vec);
+    }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-            vector<int>temp;
-            int index=0;
-            int n=candidates.size();
-            helper(candidates,ans,temp,index,target,n);
-            return ans;
+        vector<vector<int>>result;
+        vector<int>vec;
+       
+        helper(candidates,target,0,result,vec);
+        return result;
     }
 };
