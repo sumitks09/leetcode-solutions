c16ebb089e811c6e42658dcfe75e8d385f1b7c79
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-08-02 14:44:38 +0530

    Time: 701 ms (5.97%), Space: 6.1 MB (51.15%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,25 +1,32 @@
 class Solution {
+    private:
+    void helper(vector<int>vec,int k,string &s,int n)
+    {
+        for(int i=1;i<=n;i++)
+            vec.push_back(i);
+        int count=1;
+        
+        do{
+            if(count==k)
+            {
+            for(int i=0;i<vec.size();i++)
+            {
+                s+=to_string(vec[i]);
+            }
+            }
+            count++;
+        }
+        while(next_permutation(vec.begin(),vec.end()));
+    }
 public:
     string getPermutation(int n, int k) {
-            
-         vector<int> v;
-        for(int i=1; i<=n; i++)
-        {
-            v.push_back(i);
-        }
-        int t=1;
-        string s;
-        do{
-            if(t==k)
-            {
-                for(int i=0; i<n; i++)
-                    s=s+to_string(v[i]);
-            }
-            t++;
-        }
-            while(next_permutation(v.begin(), v.end()));
-               
+        vector<int>vec;
+     
+        string s="";
+        
+        helper(vec,k,s,n);
+       
         return s;
- 
+            
     }
 };
