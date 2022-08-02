917665402f24fdeb571cafa030c6a66ec9b1434a
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-08-02 11:51:41 +0530

    Time: 12 ms (64.36%), Space: 11.2 MB (57.78%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,30 +1,32 @@
 class Solution {
     private:
-    void helper(vector<int>& candidates,int target,int index,vector<vector<int>>&result,vector<int>&vec)
+    void helper(vector<int>& candidates,int index,int target,vector<vector<int>>&result,vector<int>&vec)
     {
         if(index==candidates.size())
         {
-           if(target==0)
-           {
-               result.push_back(vec);
-           }
+            if(target==0)
+            {
+                result.push_back(vec);
+            }
             return;
         }
-        
         if(candidates[index]<=target)
         {
             vec.push_back(candidates[index]);
-            helper(candidates,target-candidates[index],index,result,vec);
+            helper(candidates,index,target-candidates[index],result,vec);
             vec.pop_back();
         }
-         helper(candidates,target,index+1,result,vec);
+        helper(candidates,index+1,target,result,vec);
+            
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
         vector<vector<int>>result;
         vector<int>vec;
-       
-        helper(candidates,target,0,result,vec);
+        helper(candidates,0,target,result,vec);
         return result;
     }
 };
