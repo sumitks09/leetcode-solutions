5796723d7c6bfa7d481186fea85c7d941f1cff88
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-10-10 12:09:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     string breakPalindrome(string s) {
+     
         for(int i=0;i<s.size()/2;i++)
         {
             if(s[i]!='a')
             {
+               
                 s[i]='a';
+                
                 return s;
             }
                 
