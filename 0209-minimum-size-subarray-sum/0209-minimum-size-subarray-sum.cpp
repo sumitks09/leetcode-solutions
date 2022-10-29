9b1d718551a00d7a4f1c0379b93002d517db2efd
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-10-29 17:37:22 +0530

    Time: 105 ms (29.67%), Space: 28.3 MB (10.32%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,19 +4,17 @@ public:
         int minl=INT_MAX;
         int ps=0;
         int j=0;
-        for(int i=0;i<nums.size();i++)
-        {
+        for(int i=0;i<nums.size();i++){
             ps+=nums[i];
             while(ps>=target)
             {
                 minl=min(minl,i-j+1);
-                ps-=nums[j++];
-            }
+                ps=ps-nums[j++];
+               
                 
+            }
+            
         }
         return (minl==INT_MAX)?0:minl;
-        
-        
-        
     }
 };
