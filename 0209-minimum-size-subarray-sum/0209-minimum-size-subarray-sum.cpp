3678e7d4daf3b75a091325d34a77d6671ff4db16
Sumit Kumar singh <72199634+sumitks09@users.noreply.github.com>
2022-10-29 18:00:59 +0530

    Time: 99 ms (34.21%), Space: 28.1 MB (88.02%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int minl=INT_MAX;
-        int ps=0;
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-            ps+=nums[i];
-            while(ps>=target)
-            {
-                minl=min(minl,i-j+1);
-                ps=ps-nums[j++];
-               
-                
-            }
+       //Variable Size sliding window: 2-pointer
+        int n=nums.size();
+        int sum = 0;            //Current Sum
+        int left = 0;           //left ptr
+        int right = 0;          //right ptr
+        int shortest = INT_MAX; //shortest window with Sum>=Target
+        
+        while(right<n){
+            sum += nums[right];//Add current element to Sum
             
+            if(sum >= target){//Check if currentSum >= Target
+                //Skip all left elements till sum < target (to find the smallest window)
+                while(sum>=target){
+                    sum -= nums[left];
+                    left++;
+                }
+                shortest = min(shortest,right-left+2);//Update smallest window size
+            }
+            right++;
         }
-        return (minl==INT_MAX)?0:minl;
+        return shortest==INT_MAX?0:shortest;
     }
 };
+
+        
+        
